807f2a5645cdee3d675dedf29ddb04247e981893
eiTT-01 <xavier-leroy@outlook.com>
2024-08-22 13:33:01 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  title: '好习惯 用步鸽',
+  description: '早起 · 好睡 · 看书 · 运动 · 冥想 · 专注',
+  head: [
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }]
+  ],
+  plugins: {
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: {
+        message: "有文章更新了",
+        buttonText: "刷新"
+      }
+    },
+    '@vuepress/back-to-top': true,
+    '@vuepress/register-components': {
+      componentsDir: '/components/'
+    },
+    '@vuepress/google-analytics': {
+      'ga': 'UA-**********-*'
+    }
+  },
+  themeConfig: {
+    repo: 'ZweiRm/localhost-8080.github.io',
+    repoLabel: '查看源码',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '帮助我改善此页面！',
+    lastUpdated: '上次更新',
+    nav: [{
+      text: '主页',
+      link: '/',
+      },
+      {
+        text: '博文',
+        items: [
+          { text: 'Java', link: '/java/' },
+          { text: 'Kotlin', link: '/kotlin/' }
+        ]
+      },
+      { text: '关于', link: '/about/' },
+      { text: 'Github', link: 'https://www.github.com/ZweiRm' },
+    ],
+    sidebar: {
+      '/java/': [
+        '',
+        'grammar',
+        'object-oriented',
+        'application-programming-interface'
+      ],
+      '/kotlin/': [
+          '',
+      ]
+    },
+    sidebarDepth: 2
+  }
+}
